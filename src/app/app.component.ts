d3f2f07711859b2db54711257b5e583ffa1e7ce9
Koffi Agbenya <kagbenya@utbm.fr>
2018-10-09 11:18:33 +0200

    ajout des pizzas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pizza } from './pizza';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pizza Shop';
+  pizza: Array<Pizza> = [
+    {name:"Marguerittes", price:12, url:"https://media-cdn.tripadvisor.com/media/photo-s/0f/84/ba/ec/bon-a-pizza.jpg"},
+    {name:"royale", price:14, url:"https://www.marecetteitalienne.com/wp-content/uploads/2012/11/Garniture-pour-Pizza-Royale.jpg"}
+  ]
 }
