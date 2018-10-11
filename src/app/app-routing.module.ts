e5f3c9a70e01e6dab2362b08ddaf0f173b4b2c84
lordkevinmo <kevagb03@gmail.com>
2018-10-11 18:34:57 +0200

    add product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PizzadetailsComponent} from './pizzadetails/pizzadetails.component';
-import {PizzaComponent} from './pizza/pizza.component';
 import {PizzaListComponent} from './pizza-list/pizza-list.component';
 
 const routes: Routes = [
-    {path: '', component: PizzaListComponent},
-    {path: 'pizzas', component: PizzaComponent},
+    {path: '', redirectTo: '/pizzas', pathMatch: 'full'},
+    {path: 'pizzas', component: PizzaListComponent},
     {path: 'pizzas/:id', component: PizzadetailsComponent}
 ];
 
