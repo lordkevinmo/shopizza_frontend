d7183faa66f16a10b5ee8d78815c94221cc45b0e
lordkevinmo <kevagb03@gmail.com>
2018-10-11 12:58:43 +0200

    ajout du composant pizza détails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { PizzadetailsComponent } from './pizzadetails/pizzadetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaComponent,
-    PizzaListComponent
+    PizzaListComponent,
+    PizzadetailsComponent
   ],
   imports: [
     BrowserModule,
